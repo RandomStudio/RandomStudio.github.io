605b834f5a55464c191a2ee7d940f624c44e1ddc
Andrew Hill <andrew.hill@random.studio>
2019-09-10 17:36:09 +0200

    Quine!

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 async function getProjects () {
-  const response = await fetch('https://api.github.com/orgs/RandomStudio/projects');
+  const target = document.getElementById('target');
+  const response = await fetch('https://api.github.com/orgs/randomstudio/repos', {
+    headers: new Headers({
+      'Accept': 'application/vnd.github.baptiste-preview+json',
+    })
+  });
   const data = await response.json();
-
+  data.forEach(repo => {
+    const row = document.createElement('li');
+    row.innerHTML = `<div>${repo.name}</div><div>${repo.has_pages}</div><div>${repo.pushed_at}</div>`
+    target.append(row)
+  })
   console.log(data);
 }
 
